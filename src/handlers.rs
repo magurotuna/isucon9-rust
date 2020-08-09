ac9009e2a648f15d841ef5eabed34bcf7a5b9541
Yusuke Tanaka <yusuktan@maguro.dev>
2020-08-09 15:17:29 +0900

    Implement handlers for frontend & assets

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,8 +1,20 @@
-use tide::Result;
+use std::env;
+use tide::{Body, Result};
 
 type Request = tide::Request<crate::AppState>;
 
-pub(crate) async fn index(_req: Request) -> Result<&'static str> {
+pub(crate) async fn get_index(_req: Request) -> Result<&'static str> {
     let html = include_str!("../public/index.html");
     Ok(html)
+}
+
+pub(crate) async fn get_assets(req: Request) -> Result<Body> {
+    let mut file_path = env::current_dir()?;
+    let path: String = req.param("path")?;
+    file_path.push("public");
+    file_path.push(path);
+
+    // TODO: return 404 if the file does not exist
+    let body = Body::from_file(&file_path).await?;
+    Ok(body)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use anyhow::{Context, Result};
 use std::env;
-use anyhow::{Result, Context};
 
-mod handlers;
 mod consts;
+mod handlers;
 
 #[async_std::main]
 async fn main() -> Result<()> {
@@ -10,12 +10,16 @@ async fn main() -> Result<()> {
 
     let host = env::var("MYSQL_HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
     let port = env::var("MYSQL_PORT").unwrap_or_else(|_| "3306".to_string());
-    port.parse::<i32>().context("failed to read DB port number from an environment variable MYSQL_PORT.")?;
+    port.parse::<i32>()
+        .context("failed to read DB port number from an environment variable MYSQL_PORT.")?;
     let user = env::var("MYSQL_USER").unwrap_or_else(|_| "isucari".to_string());
     let dbname = env::var("MYSQL_DBNAME").unwrap_or_else(|_| "isucari".to_string());
     let password = env::var("MYSQL_PASS").unwrap_or_else(|_| "isucari".to_string());
 
-    let dsn = format!("{}:{}@tcp({}:{})/{}?charset=utf8mb4&parseTime=true&loc=Local", user, password, host, port, dbname);
+    let dsn = format!(
+        "{}:{}@tcp({}:{})/{}?charset=utf8mb4&parseTime=true&loc=Local",
+        user, password, host, port, dbname
+    );
     let conn = connect(&dsn).await?;
     let state = AppState { conn };
 
@@ -24,7 +28,24 @@ async fn main() -> Result<()> {
     // TODO
 
     // Frontend
-    app.at("/").get(handlers::index);
+    app.at("/").get(handlers::get_index);
+    app.at("/login").get(handlers::get_index);
+    app.at("/register").get(handlers::get_index);
+    app.at("/timeline").get(handlers::get_index);
+    app.at("/categories/:category_id/items")
+        .get(handlers::get_index);
+    app.at("/sell").get(handlers::get_index);
+    app.at("/items/:item_id").get(handlers::get_index);
+    app.at("/items/:item_id/edit").get(handlers::get_index);
+    app.at("/items/:item_id/buy").get(handlers::get_index);
+    app.at("/buy/complete").get(handlers::get_index);
+    app.at("/transactions/:transaction_id")
+        .get(handlers::get_index);
+    app.at("/users/:user_id").get(handlers::get_index);
+    app.at("/users/setting").get(handlers::get_index);
+
+    // Assets
+    app.at("/*path").get(handlers::get_assets);
 
     app.listen("127.0.0.1:8080").await?;
     Ok(())
