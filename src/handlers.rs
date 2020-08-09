51f6ee44d2cd7f029ab17154d2552e33cd6ea610
Yusuke Tanaka <yusuktan@maguro.dev>
2020-08-09 20:34:50 +0900

    Implement GET /new_items.json

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,9 +1,16 @@
-use crate::models::{ReqInitialize, ResInitialize};
+use crate::consts;
+use crate::models::{
+    Category, Item, ItemSimple, ReqInitialize, ResInitialize, ResNewItems, UserSimple,
+};
 use crate::AppState;
+use anyhow::Result as AnyhowResult;
+use serde::Deserialize;
+use sqlx::mysql::MySqlQueryAs;
+use sqlx::MySqlPool;
 use std::env;
 use std::io::{self, Write};
 use std::process::Command;
-use tide::{Body, Result, StatusCode};
+use tide::{Body, Result as TideResult, StatusCode};
 
 type Request = tide::Request<AppState>;
 
@@ -18,7 +25,7 @@ where
     }
 }
 
-pub(crate) async fn post_initialize(mut req: Request) -> Result<Body> {
+pub(crate) async fn post_initialize(mut req: Request) -> TideResult<Body> {
     let body: ReqInitialize = req
         .body_json()
         .await
@@ -67,76 +74,190 @@ pub(crate) async fn post_initialize(mut req: Request) -> Result<Body> {
     Ok(Body::from_json(&res)?)
 }
 
-pub(crate) async fn get_new_items(req: Request) -> Result<String> {
+pub(crate) async fn get_new_items(req: Request) -> TideResult<Body> {
+    #[derive(Deserialize, Default)]
+    #[serde(default)]
+    struct Query {
+        item_id: u64,
+        created_at: u64,
+    }
+    let query: Query = req.query().map_err(with_status(StatusCode::BadRequest))?;
+    let Query {
+        item_id,
+        created_at,
+    } = query;
+
+    let conn = &req.state().conn;
+    let items: Vec<Item> = if item_id > 0 && created_at > 0 {
+        sqlx::query_as(
+            r"
+            SELECT 
+                id,
+                seller_id,
+                buyer_id,
+                status,
+                name,
+                price,
+                description,
+                image_name,
+                category_id,
+                created_at,
+                updated_at
+            FROM `items`
+            WHERE `status` IN (?,?)
+            AND (`created_at` < ? OR (`created_at` <= ? AND `id` < ?))
+            ORDER BY `created_at` DESC, `id` DESC
+            LIMIT ?
+            ",
+        )
+        .bind(consts::ITEM_STATUS_ON_SALE)
+        .bind(consts::ITEM_STATUS_SOLD_OUT)
+        .bind(created_at)
+        .bind(created_at)
+        .bind(item_id)
+        .bind(consts::ITEMS_PER_PAGE + 1)
+        .fetch_all(conn)
+        .await?
+    } else {
+        sqlx::query_as(
+            r"
+            SELECT 
+                id,
+                seller_id,
+                buyer_id,
+                status,
+                name,
+                price,
+                description,
+                image_name,
+                category_id,
+                created_at,
+                updated_at
+            FROM `items`
+            WHERE `status` IN (?,?)
+            ORDER BY `created_at` DESC, `id` DESC
+            LIMIT ?
+            ",
+        )
+        .bind(consts::ITEM_STATUS_ON_SALE)
+        .bind(consts::ITEM_STATUS_SOLD_OUT)
+        .bind(consts::ITEMS_PER_PAGE + 1)
+        .fetch_all(conn)
+        .await?
+    };
+
+    let mut item_simples = Vec::new();
+    for item in items {
+        let seller = get_user_simple_by_id(conn, item.seller_id).await?;
+        let category = get_category_by_id(conn, item.category_id).await?;
+        item_simples.push(ItemSimple {
+            id: item.id,
+            seller_id: item.seller_id,
+            seller,
+            status: item.status,
+            name: item.name,
+            price: item.price,
+            image_url: get_image_url(item.image_name),
+            category_id: item.category_id,
+            category,
+            created_at: item.created_at.timestamp(),
+        });
+    }
+
+    let mut has_next = false;
+    if item_simples.len() > consts::ITEMS_PER_PAGE as usize {
+        has_next = true;
+        item_simples.truncate(consts::ITEMS_PER_PAGE as usize);
+    }
+
+    let res = ResNewItems {
+        root_category_id: None,
+        root_category_name: None,
+        has_next,
+        items: item_simples,
+    };
+
+    Ok(Body::from_json(&res)?)
+}
+
+async fn get_user_simple_by_id(conn: &MySqlPool, seller_id: i64) -> AnyhowResult<UserSimple> {
     todo!()
 }
 
-pub(crate) async fn get_root_category_id(req: Request) -> Result<String> {
+async fn get_category_by_id(conn: &MySqlPool, category_id: i32) -> AnyhowResult<Category> {
     todo!()
 }
 
-pub(crate) async fn get_transactions(req: Request) -> Result<String> {
+fn get_image_url(image_name: String) -> String {
+    format!("/upload/{}", image_name)
+}
+
+pub(crate) async fn get_root_category_id(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn get_item_id(req: Request) -> Result<String> {
+pub(crate) async fn get_transactions(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn post_item_edit(req: Request) -> Result<String> {
+pub(crate) async fn get_item_id(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn post_buy(req: Request) -> Result<String> {
+pub(crate) async fn post_item_edit(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn post_sell(req: Request) -> Result<String> {
+pub(crate) async fn post_buy(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn post_ship(req: Request) -> Result<String> {
+pub(crate) async fn post_sell(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn post_ship_done(req: Request) -> Result<String> {
+pub(crate) async fn post_ship(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn post_complete(req: Request) -> Result<String> {
+pub(crate) async fn post_ship_done(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn get_qr_code(req: Request) -> Result<String> {
+pub(crate) async fn post_complete(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn post_bump(req: Request) -> Result<String> {
+pub(crate) async fn get_qr_code(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn get_settings(req: Request) -> Result<String> {
+pub(crate) async fn post_bump(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn post_login(req: Request) -> Result<String> {
+pub(crate) async fn get_settings(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn post_register(req: Request) -> Result<String> {
+pub(crate) async fn post_login(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn get_reports(req: Request) -> Result<String> {
+pub(crate) async fn post_register(req: Request) -> TideResult<String> {
     todo!()
 }
 
-pub(crate) async fn get_index(_req: Request) -> Result<&'static str> {
+pub(crate) async fn get_reports(req: Request) -> TideResult<String> {
+    todo!()
+}
+
+pub(crate) async fn get_index(_req: Request) -> TideResult<&'static str> {
     let html = include_str!("../public/index.html");
     Ok(html)
 }
 
-pub(crate) async fn get_assets(req: Request) -> Result<Body> {
+pub(crate) async fn get_assets(req: Request) -> TideResult<Body> {
     let mut file_path = env::current_dir()?;
     let path: String = req.param("path")?;
     file_path.push("public");
--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,9 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use sqlx::mysql::MySqlRow;
+use sqlx::{FromRow, Row};
+
+type Time = DateTime<Utc>;
 
 #[derive(Deserialize)]
 pub(crate) struct ReqInitialize {
@@ -10,4 +15,84 @@ pub(crate) struct ReqInitialize {
 pub(crate) struct ResInitialize {
     pub(crate) campaign: i32,
     pub(crate) language: String,
+}
+
+pub(crate) struct Item {
+    pub(crate) id: i64,
+    pub(crate) seller_id: i64,
+    pub(crate) buyer_id: i64,
+    pub(crate) status: String,
+    pub(crate) name: String,
+    pub(crate) price: i32,
+    pub(crate) description: String,
+    pub(crate) image_name: String,
+    pub(crate) category_id: i32,
+    pub(crate) created_at: Time,
+    pub(crate) updated_at: Time,
+}
+
+impl<'c> FromRow<'c, MySqlRow<'c>> for Item {
+    fn from_row(row: &MySqlRow) -> Result<Self, sqlx::Error> {
+        let id: i64 = row.try_get("id")?;
+        let seller_id: i64 = row.try_get("seller_id")?;
+        let buyer_id: i64 = row.try_get("buyer_id")?;
+        let status: String = row.try_get("status")?;
+        let name: String = row.try_get("name")?;
+        let price: i32 = row.try_get("price")?;
+        let description: String = row.try_get("description")?;
+        let image_name: String = row.try_get("image_name")?;
+        let category_id: i32 = row.try_get("category_id")?;
+        let created_at: Time = row.try_get("created_at")?;
+        let updated_at: Time = row.try_get("updated_at")?;
+        Ok(Item {
+            id,
+            seller_id,
+            buyer_id,
+            status,
+            name,
+            price,
+            description,
+            image_name,
+            category_id,
+            created_at,
+            updated_at,
+        })
+    }
+}
+
+#[derive(Serialize)]
+pub(crate) struct UserSimple {
+    pub(crate) id: i64,
+    pub(crate) account_name: String,
+    pub(crate) num_sell_items: i32,
+}
+
+#[derive(Serialize)]
+pub(crate) struct ItemSimple {
+    pub(crate) id: i64,
+    pub(crate) seller_id: i64,
+    pub(crate) seller: UserSimple,
+    pub(crate) status: String,
+    pub(crate) name: String,
+    pub(crate) price: i32,
+    pub(crate) image_url: String,
+    pub(crate) category_id: i32,
+    pub(crate) category: Category,
+    pub(crate) created_at: i64,
+}
+
+#[derive(Serialize)]
+pub(crate) struct Category {
+    pub(crate) id: i32,
+    pub(crate) parent_id: i32,
+    pub(crate) category_name: String,
+    pub(crate) parent_category_name: String,
+}
+
+#[derive(Serialize)]
+pub(crate) struct ResNewItems {
+    pub(crate) root_category_id: Option<i32>,
+    pub(crate) root_category_name: Option<String>,
+    pub(crate) has_next: bool,
+    pub(crate) items: Vec<ItemSimple>,
 }
